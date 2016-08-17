ff9ac1b0ab5ce195a5b8e5c5e25e2c6631dd51d6
OREDIS-VP\fbellahcene <fbellahcene@vente-privee.com>
2016-08-17 12:09:22 +0200

    create base classes

---
+++ TfsToCsv/TfsToCsv/Field.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace TfsToCsv
+{
+    public class Field
+    {
+        [XmlAttribute]
+        public string RefName { get; set; }
+
+        [XmlAttribute]
+        public string Value { get; set; }
+    }
+}
---
+++ TfsToCsv/TfsToCsv/WorkItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TfsToCsv
+{
+    public class WorkItem
+    {
+
+        [XmlAttribute]
+        public int Id { get; set; }
+
+        [XmlElement]
+        public List<Field> Fields { get; set; }
+    }
+}
